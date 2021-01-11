dfdd1ed161d49c1336970af35f17c303d293bac6
ivanmijacika <imijacika20@stuy.edu>
2021-01-11 13:23:19 -0500

    length first attempt added

--- Radix.java
+++ Radix.java
@@ -5,9 +5,11 @@ public class Radix{
     return q%10;
   }
 
-  //public static int length(int n){
-
-  //}
+  public static int length(int n){
+    if (n==0) return 1;
+    int l = (int) (Math.log10(n) + 1);
+    return l;
+  }
 
 
 }
