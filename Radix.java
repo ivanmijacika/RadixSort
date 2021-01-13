901ebb7c8a165dd84cc2bd80204b4e88d6340ddd
ivanmijacika <imijacika20@stuy.edu>
2021-01-13 00:41:36 -0500

    attempted RadixSort but couldn't figure out last part

--- Radix.java
+++ Radix.java
@@ -40,4 +40,32 @@ public class Radix{
     }
   }
 
+  public static void radixSort(SortableLinkedList data){
+    SortableLinkedList[] buckets = new SortableLinkedList[10];
+    SortableLinkedList[] posneg = new SortableLinkedList[2]; //+ or - distnguisher
+    int maxLength = 0;
+    for (int i=0; i<10; i++){
+      buckets[i] = new SortableLinkedList();
+    }
+    for (int i=0; i<2; i++){
+      posneg[i] = new SortableLinkedList();
+    }
+    while(data.size()>0){
+      if(length(data.get(0)) > maxLength){
+        maxLength = length(data.get(0));
+      }
+      buckets[nth(data.get(0), 0)].add(data.get(0));
+      data.remove(0);
+    }
+    merge(data, buckets);
+    for (int i=1; i<maxLength; i++){
+      while(data.size()>0){
+        buckets[nth(data.get(0), i)].add(data.get(0));
+        data.remove(0);
+      }
+      merge(data, buckets);
+    }
+    //???? Something to do with signs???
+  }
+
 }
